2dd77358a2ff8c93fceb1a336f4fe257acdb80a4
IPreferGodot <marius.bardel@csilyon.fr>
2023-04-19 19:11:13 +0200

    Premier jet mot de passe

---
+++ docs/common/password.js
@@ -0,0 +1,19 @@
+// This script hold the behavior of the password field to grant easter eggs
+
+import { EASTER_EGGS_MANAGER } from "./easter_eggs.js";
+
+const STRICT_PASSWORDS = {
+    // password: easter_egg_id,
+    "un mot de passe": "placeholder_pwd",
+    
+};
+
+
+function attempt(password) {
+    let toUnlock = STRICT_PASSWORDS[password];
+    if (toUnlock != undefined) {
+        EASTER_EGGS_MANAGER.unlock(toUnlock)
+    }
+    
+    // TODO implémenter pour les mots de passe non strict (qui acceptent de petites erreurs)
+};
